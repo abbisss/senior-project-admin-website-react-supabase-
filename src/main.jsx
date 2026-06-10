bb456966d832aad1b75726670b39587106f30ba8
Abbass <abbasscodingpc2@gmail.com>
2026-06-10 14:45:24 +0300

    added scroll to top on path change

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import App from './App'
+import ScrollToTop from './routes/ScrollToTop';
+
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+     <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>
