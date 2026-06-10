16bf31d999a60645ffacfd98f56916339894943b
Abbass <abbasscodingpc2@gmail.com>
2026-06-10 17:21:39 +0300

    try

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <div className="app-wrapper">
-      <Navbar />
+      
 
       <div className="app-content">
         <Outlet />
